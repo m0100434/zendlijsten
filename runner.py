da560a477370df54de37766a1e250855f640ac94
Maarten Verschuere <m.verschuere@ailvis.com>
2021-03-31 17:36:02 +0200

    march complete

--- runner.py
+++ runner.py
@@ -1,9 +1,10 @@
 import os
 
-os.system('main_210320.py')
-os.system('main_210322.py')
-os.system('main_210323.py')
-#os.system('main_210319.py')
-#os.system('main_210320.py')
+os.system('main_210328.py')
+os.system('main_210329.py')
+os.system('main_210330.py')
+os.system('main_210331.py')
+os.system('main_210401.py')
+os.system('main_210402.py')
 
 
