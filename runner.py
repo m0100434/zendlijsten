15dd43288fde5016e871ec4eb84f836b5794541e
Maarten Verschuere <m.verschuere@ailvis.com>
2021-04-03 22:13:40 +0200

    210406

--- runner.py
+++ runner.py
@@ -1,10 +1,11 @@
 import os
 
-os.system('main_210328.py')
-os.system('main_210329.py')
-os.system('main_210330.py')
-os.system('main_210331.py')
-os.system('main_210401.py')
+
 os.system('main_210402.py')
+os.system('main_210403.py')
+os.system('main_210404.py')
+os.system('main_210405.py')
+os.system('main_210406.py')
+
 
 
