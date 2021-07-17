171e1c427db71dad01408072081c85035c57a2b2
Maarten Verschuere <m.verschuere@ailvis.com>
2021-07-17 09:51:46 +0200

    1707

--- runner.py
+++ runner.py
@@ -1,7 +1,15 @@
 import os
 
-os.system('main_210503.py')
-os.system('main_210504.py')
-os.system('main_210505.py')
-os.system('main_210506.py')
-os.system('main_210507.py')
+
+
+#os.system('main_210707.py')
+#os.system('main_210708.py')
+#os.system('main_210709.py')
+#os.system('main_210710.py')
+os.system('main_210715.py')
+os.system('main_210716.py')
+os.system('main_210717.py')
+os.system('main_210718.py')
+os.system('main_210719.py')
+os.system('main_210720.py')
+os.system('main_210721.py')
